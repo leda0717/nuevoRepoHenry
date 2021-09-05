a2598da821f674dce9a73c9e82d8e90228177c1c
Leda Quiroz Garcia <ledaqg17@gmail.com>
2021-09-05 18:15:45 -0500

    ejemploarray

--- archivo.js
+++ archivo.js
@@ -1,3 +1,36 @@
 function nombre (){
     return 'Hola!'
 }
+
+let alumnos = [
+    {
+        nombre: 'Emi',
+        edad: 23,
+        hobbies: ['correr', 'dormir', 'nadar'],
+        amigos: [
+            {
+                nombre: 'Martin'
+            },
+            {
+                nombre: 'Leo'
+            }
+        ],
+    },
+    {
+        nombre: 'Franco',
+        edad: 21, 
+        hobbies: ['futbol', 'golf'],
+        amigos: [
+            {
+                nombre: 'Emi',
+            },
+            {
+                nombre: 'Jimmy',
+            },
+            {
+                nombre: 'Matias',
+            }
+        ],
+
+    },
+]
